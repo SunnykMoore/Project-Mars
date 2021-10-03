cb8d19f6105dacd0a46cbdf4a30e75af1597ad73
ToasterExplosion <rbess@memphis.edu>
2021-10-03 16:34:07 -0500

    We now have a landing page, shitty as it is. I'm new to HTML back off me. Also made a 'products' and 'order form' page

--- HelloDjango/HelloDjango/urls.py
+++ HelloDjango/HelloDjango/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url,include
 from django.urls import path
+from pages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('products/', views.products, name='catalogue'),
+    path('order/', views.order, name='orderform'),
+    
 ]
