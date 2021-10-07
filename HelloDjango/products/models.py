0413504fd8d7f4bb9f6afcfdf1c95058c216dc2c
ToasterExplosion <rbess@memphis.edu>
2021-10-07 14:59:24 -0500

    updated the product creation model in admin

--- HelloDjango/products/models.py
+++ HelloDjango/products/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
 class Product(models.Model):
 	name = models.TextField()
 	description = models.TextField()
-	price = models.TextField()
-	image = models.ImageField()
-	tags = {} #potential later feature, so not all the keywors have to be in the description
+	price = models.DecimalField(max_digits=100, decimal_places=2)
+	image = models.ImageField(default="default.jpeg")
+	product_id = models.IntegerField(default=0000)
+	category = models.TextField(default = "")
