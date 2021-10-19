83e79d66b835426fe886731faf883e838cdb6d4a
Brett Edmonds <notquitealibrarian@gmail.com>
2021-10-18 20:15:10 -0500

    Added default Create Order view, created form html file to capture order info, added appropriate urls.  Also added depreciated form.py file which will be bypassed using django's CreateView

--- HelloDjango/HelloDjango/urls.py
+++ HelloDjango/HelloDjango/urls.py
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.conf.urls import url,include
 from django.urls import path
 from pages import views
-from orders.views import OrderListView
+from orders.views import OrderListView, CreateOrderView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('products/', views.products, name='catalogue'),
     #path('order/', views.order, name='orderform'),
-    path('orders/', OrderListView.as_view()),
+    path('orders/', OrderListView.as_view(), name='orders'),
+    path('order/create/', CreateOrderView.as_view(), name='create_order'),
 ]
--- HelloDjango/orders/views.py
+++ HelloDjango/orders/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from orders.models import Order
+from orders.forms import OrderForm
 
 class OrderListView(ListView):
     model = Order
+
+class CreateOrderView(CreateView):
+    model = Order
+    # form_class = OrderForm
+    fields = '__all__'
+    success_url = '/orders/'
