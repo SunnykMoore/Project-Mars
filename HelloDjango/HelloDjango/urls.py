01de683bd173fb5be160b5a4c2d91a087c611e1e
colonelpanic51 <77797347+colonelpanic51@users.noreply.github.com>
2021-10-19 15:42:47 -0500

    reverted urls.py to version on main

--- HelloDjango/HelloDjango/urls.py
+++ HelloDjango/HelloDjango/urls.py
@@ -17,13 +17,13 @@ from django.contrib import admin
 from django.conf.urls import url,include
 from django.urls import path
 from pages import views
-from products import views as prod #gotta rename it so it won't conflict with pages/views
+from orders.views import OrderListView, CreateOrderView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('products/', prod.catalogView, name='catalogue'),
-    path('products/<prodID>', prod.prodView, name='product'),
-    path('order/', views.order, name='orderform'),
-    
+    path('products/', views.products, name='catalogue'),
+    #path('order/', views.order, name='orderform'), # Depreciated but kept for reference 10.18.21 BE
+    path('orders/', OrderListView.as_view(), name='orders'),
+    path('order/create/', CreateOrderView.as_view(), name='create_order'),
 ]
