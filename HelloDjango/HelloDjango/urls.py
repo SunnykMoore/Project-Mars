e23c433ba031b2d554c62e5926e839b95e81697c
Brett Edmonds <notquitealibrarian@gmail.com>
2021-10-19 22:30:09 -0500

    Still fixing...

--- HelloDjango/HelloDjango/urls.py
+++ HelloDjango/HelloDjango/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='catalogue'),
     #path('order/', views.order, name='orderform'), # Depreciated but kept for reference 10.18.21 BE
-    path('orders', OrderListView.as_view(), name='orders'),
+    path('orders/', OrderListView.as_view(), name='orders'),
     path('order/create/', CreateOrderView.as_view(), name='create_order'),
 ]
