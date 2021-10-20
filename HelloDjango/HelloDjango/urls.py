9311c179b8d5eeb8760e900504945df7bd3fc239
Brett Edmonds <notquitealibrarian@gmail.com>
2021-10-19 22:21:15 -0500

    moved some urls to inside orders directory.

--- HelloDjango/HelloDjango/urls.py
+++ HelloDjango/HelloDjango/urls.py
@@ -24,7 +24,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='catalogue'),
     #path('order/', views.order, name='orderform'), # Depreciated but kept for reference 10.18.21 BE
-    path('orders/', OrderListView.as_view(), name='orders'),
-    path('order/create/', CreateOrderView.as_view(), name='create_order'),
-    path('orders/in_progress', views.OrderInProgress, name='orders_in_progress'),
 ]
