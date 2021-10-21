1a2bbcdcb83bc6d2ec08a91f5f26a8469a835724
ToasterExplosion <rbess@memphis.edu>
2021-10-20 23:13:54 -0500

    minor formatting change, still can't get image to show or fields to auto-fill in orders but we get there when we get there

--- HelloDjango/HelloDjango/urls.py
+++ HelloDjango/HelloDjango/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     #path('order/', views.order, name='orderform'), # Depreciated but kept for reference 10.18.21 BE
     path('orders/', OrderListView.as_view(), name='orders'),
     path('order/create/', CreateOrderView.as_view(), name='create_order'),
+    path('products/<prodID>/order/create/', CreateOrderView.as_view(), name='create_order'),
 ]
