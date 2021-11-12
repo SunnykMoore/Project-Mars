7d8fcacf66e7c053909a691eb09ac8b5f611452f
Brett Edmonds <notquitealibrarian@gmail.com>
2021-11-11 18:23:11 -0600

    added some comments to code to make it more readable by other developers.

--- HelloDjango/orders/views.py
+++ HelloDjango/orders/views.py
@@ -35,24 +35,25 @@ class OrderFromCatalog(CreateView, Product): #foundation technically there
 class OrderDetailView(DetailView):
     model = Order
 
-class OrderCopyView(UpdateView): # Reorders
-    model = Order
+class OrderCopyView(UpdateView): # Reorders, inherits from the generic django update view
+    model = Order #Selects the model to use this view action on
     
-    def get_object(self, queryset=None):
-        old_item = super().get_object(queryset)
-        old_item.is_reordered = True
-        old_item.num_reorders = (old_item.num_reorders + 1)
-        old_item.save()
-        new_item = super().get_object(queryset)
-        new_item.pk = None
-        new_item.is_reordered = False
+    def get_object(self, queryset=None): #overrides inherent get_object method in UpdateView
+        # if old_item.parent_reorder is None: WHEN TO SET THIS UP?
+        old_item = super().get_object(queryset) #sets the old order by calling the generic get_object version of the method
+        old_item.is_reordered = True  #sets the old order as a reorder
+        old_item.num_reorders = (old_item.num_reorders + 1) #increases the number of times this reorder has been placed
+        old_item.save() #saves to database
+        new_item = super().get_object(queryset) #grabs the old order again, but sets to new order variable
+        new_item.pk = None #removes primary key, which causes django to make a new object and assign a new pk
+        new_item.is_reordered = False #lines 48-49 may not be needed, but wanted to make sure new order didn't initialize its reorder fields incorrectly.
         new_item.num_reorders = 0
-        new_item.parent_reorder = Order.objects.get(pk=old_item.pk)
-        new_item.save()
-        return new_item
+        new_item.parent_reorder = Order.objects.get(pk=old_item.pk) #Sets new order's parent as old order
+        new_item.save() #saves to DB
+        return new_item # returns the grabbed object (the newly created order) to the UpdateView to act on
     
     fields = ('product', 'SR_first_name', 'SR_last_name', 'SR_phone_number', 'SM_first_name', 'SM_last_name',
         'SR_email', 'department', 'physician', 'hospital', 'customer_type', 'clinical_need',
         'instrument_category', 'description', 'size', 'quantity', 'disclaimer', 'instrument_type',
-        'instrument_handle', 'status')
+        'instrument_handle', 'status') #Selects fields to show in the form (not showing those that deal with reorders)
     success_url = '/orders/'
