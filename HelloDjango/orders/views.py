d242e536091ccc4a7c21f5bbcb590220c3a2ec67
ToasterExplosion <rbess@memphis.edu>
2021-11-13 09:34:09 -0600

    New function that pulls the correct product id but the class is TOO STUPID TO USE A GLOBAL VARIABLE

--- HelloDjango/orders/views.py
+++ HelloDjango/orders/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.views.generic import ListView, CreateView
 from orders.models import Order
 from orders.forms import OrderForm
 from products.models import Product
 from django.http import HttpResponse
+import django.template
 
 class OrderListView(ListView):
     model = Order
@@ -17,14 +19,22 @@ class CreateOrderView(CreateView):
 class CurrentOrderListView(ListView):
     model = Order
   
-class OrderFromCatalog(CreateView, Product): #foundation technically there
+class OrderFromCatalog(CreateView): #The pre-filled order form for each catalog product
     model = Order
-    prodID = Product.product_id
-    print(Product)
-    prod = Product.objects.get(product_id=1)
-    # form_class = OrderForm
+    def get_context_data(self, **kwargs): #Gets the <prodID> from the url that we need
+    	context = super(OrderFromCatalog, self).get_context_data(**kwargs)
+    	context['prodID']= self.kwargs['prodID']
+    	global prod
+    	prod = Product.objects.get(product_id=context['prodID'])
+    	#it actually gets the correct product based on the prodID,
+    	#BUT IT WON'T FUCKING PASS IT OUTSIDE THE FUNCTION
+    	#IT'S A GLOBAL VARIABLE WHAT THE FUCK
+    	return context
+    	
     fields = '__all__'
-    initial = { 'product': prod,
+    
+    prod = Product.objects.get(product_id=1) #placeholder since GLOBAL WON'T COOPERATE
+    initial = { 'product': prod,	#Sets up initial values to fill fields
     'size': prod.size,
     'instrument_category': prod.category,
     'description': prod.description,
@@ -33,3 +43,5 @@ class OrderFromCatalog(CreateView, Product): #foundation technically there
     'instrument_handle': prod.handle,
     }
     success_url = '/orders/'
+
+    
