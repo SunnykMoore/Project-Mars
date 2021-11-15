ce54d7888925df1388998a35f0237a33253e03ee
Brett Edmonds <notquitealibrarian@gmail.com>
2021-11-14 18:27:30 -0600

    Now trying by inheriting from UpdateView, using get_or_create, and get_object methods.

--- HelloDjango/orders/views.py
+++ HelloDjango/orders/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from orders.models import Order
 from orders.forms import OrderForm
 from products.models import Product
@@ -19,7 +19,7 @@ class CreateOrderView(CreateView):
 class CurrentOrderListView(ListView):
     model = Order
   
-class OrderFromCatalog(CreateView): #The pre-filled order form for each catalog product
+class OrderFromCatalog(UpdateView): #The pre-filled order form for each catalog product
     model = Order
     
     fields = ('product', 'SR_first_name', 'SR_last_name', 'SR_phone_number', 'SM_first_name', 'SM_last_name',
@@ -27,16 +27,15 @@ class OrderFromCatalog(CreateView): #The pre-filled order form for each catalog 
         'instrument_category', 'description', 'size', 'quantity', 'disclaimer', 'instrument_type',
         'instrument_handle', 'status')
     
-    def get_initial(self, *args, **kwargs):
-        initial = super(OrderFromCatalog, self).get_initial(**kwargs)
-        initial['prodID'] = self.kwargs['prodID']
-        prod = Product.objects.get(product_id=initial['prodID'])
-        initial['size'] = prod.size
-        initial['category'] = prod.category
-        initial['description'] = prod.description
-        initial['product_type'] = prod.product_type
-        initial['handle'] = prod.handle
-        return initial
+    def get_object(self, queryset=None):
+        order, created = Order.objects.get_or_create(pk = self.kwargs['pk'])
+        prod = Product.objects.get(product_id=self.kwargs['prodID'])
+        order.size = prod.size
+        order.instrument_category=prod.category
+        order.description = prod.description
+        order.instrument_type = prod.product_type
+        order.instrument_handle = prod.handle
+        return order
     
     """
     def get_context_data(self, **kwargs): #Gets the <prodID> from the url that we need
@@ -48,6 +47,8 @@ class OrderFromCatalog(CreateView): #The pre-filled order form for each catalog 
     	#BUT IT WON'T FUCKING PASS IT OUTSIDE THE FUNCTION
     	#IT'S A GLOBAL VARIABLE WHAT THE FUCK
     	return context
+    
+    
     initial = { 'product': prod,	#Sets up initial values to fill fields
     'size': prod.size,
     'instrument_category': prod.category,
@@ -57,6 +58,7 @@ class OrderFromCatalog(CreateView): #The pre-filled order form for each catalog 
     'instrument_handle': prod.handle,
     }
     """
+    
     success_url = '/orders/'
 
     
