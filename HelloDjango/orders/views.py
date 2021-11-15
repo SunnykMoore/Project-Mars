e72f5043fce3dd08a00bf53b07eee7ce6e71d702
Brett Edmonds <notquitealibrarian@gmail.com>
2021-11-14 19:42:01 -0600

    Added comments to code for readability.  Removed nulls allowed from orders, have a hunch new implementation will work without allowing null values.

--- HelloDjango/orders/views.py
+++ HelloDjango/orders/views.py
@@ -20,45 +20,23 @@ class CurrentOrderListView(ListView):
     model = Order
   
 class OrderFromCatalog(UpdateView): #The pre-filled order form for each catalog product
-    model = Order
+    model = Order #select model to create/update objects with
     
     fields = ('product', 'SR_first_name', 'SR_last_name', 'SR_phone_number', 'SM_first_name', 'SM_last_name',
         'SR_email', 'department', 'physician', 'hospital', 'customer_type', 'clinical_need',
         'instrument_category', 'description', 'size', 'quantity', 'disclaimer', 'instrument_type',
-        'instrument_handle', 'status')
+        'instrument_handle', 'status') #Select fields to show on form
     
-    def get_object(self, queryset=None):
-        order = Order()
-        prod = Product.objects.get(product_id=self.kwargs.get('prodID'))
-        order.product = prod
+    def get_object(self, queryset=None): #override inherent get_object method
+        order = Order() #Instead of selecting an existing object, create brand new one
+        prod = Product.objects.get(product_id=self.kwargs.get('prodID')) #get the product using the prodID section of the url
+        order.product = prod #lines 33-38 set the attributes of the order based on the selected product
         order.size = prod.size
         order.instrument_category=prod.category
         order.description = prod.description
         order.instrument_type = prod.product_type
         order.instrument_handle = prod.handle
         return order
-    
-    """
-    def get_context_data(self, **kwargs): #Gets the <prodID> from the url that we need
-    	context = super(OrderFromCatalog, self).get_context_data(**kwargs)
-    	context['prodID']= self.kwargs['prodID']
-    	global prod
-    	prod = Product.objects.get(product_id=context['prodID'])
-    	#it actually gets the correct product based on the prodID,
-    	#BUT IT WON'T FUCKING PASS IT OUTSIDE THE FUNCTION
-    	#IT'S A GLOBAL VARIABLE WHAT THE FUCK
-    	return context
-    
-    
-    initial = { 'product': prod,	#Sets up initial values to fill fields
-    'size': prod.size,
-    'instrument_category': prod.category,
-    'description': prod.description,
-    'quantity': 1,
-    'instrument_type': prod.product_type,
-    'instrument_handle': prod.handle,
-    }
-    """
     
     success_url = '/orders/'
 
