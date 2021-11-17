fdd84cd0fc12f6162359e1c1dac5d68d80e53b9a
Brett Edmonds <notquitealibrarian@gmail.com>
2021-11-16 20:07:10 -0600

    Merging in MARS-50.

--- HelloDjango/HelloDjango/urls.py
+++ HelloDjango/HelloDjango/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.conf.urls import url,include
 from django.urls import path
 from pages import views
-from orders.views import OrderListView, CreateOrderView, OrderDetailView, OrderCopyView
+from orders.views import OrderFromCatalog, OrderListView, CreateOrderView, OrderDetailView, OrderCopyView
 from products import views as prod
 
 urlpatterns = [
@@ -28,7 +28,7 @@ urlpatterns = [
     #path('order/', views.order, name='orderform'), # Depreciated but kept for reference 10.18.21 BE
     path('orders/', OrderListView.as_view(), name='orders'),
     path('order/create/', CreateOrderView.as_view(), name='create_order'),
-    path('products/<prodID>/order/create/', CreateOrderView.as_view(), name='create_order'),
+    path('products/<prodID>/order/create/', OrderFromCatalog.as_view(), name='create_order'),
     path('orders/<int:pk>', OrderDetailView.as_view(), name='order_detail'),
     path('orders/<pk>/reorder', OrderCopyView.as_view(), name='reorder')
 ]
--- HelloDjango/orders/views.py
+++ HelloDjango/orders/views.py
@@ -18,15 +18,25 @@ class CreateOrderView(CreateView):
 class CurrentOrderListView(ListView):
     model = Order
   
-class CreateOrderView(CreateView, Product): #foundation technically there
-    model = Order
-    # form_class = OrderForm
-    fields = '__all__'
-    product = Product
-    size = product.size
-    instrument_handle = product.handle
-    instrument_type = product.product_type
-    instrument_category = product.category
+class OrderFromCatalog(UpdateView): #The pre-filled order form for each catalog product
+    model = Order #select model to create/update objects with
+    
+    fields = ('product', 'SR_first_name', 'SR_last_name', 'SR_phone_number', 'SM_first_name', 'SM_last_name',
+        'SR_email', 'department', 'physician', 'hospital', 'customer_type', 'clinical_need',
+        'instrument_category', 'description', 'size', 'quantity', 'disclaimer', 'instrument_type',
+        'instrument_handle', 'status') #Select fields to show on form
+    
+    def get_object(self, queryset=None): #override inherent get_object method
+        order = Order() #Instead of selecting an existing object, create brand new one
+        prod = Product.objects.get(product_id=self.kwargs.get('prodID')) #get the product using the prodID section of the url
+        order.product = prod #lines 33-38 set the attributes of the order based on the selected product
+        order.size = prod.size
+        order.instrument_category=prod.category
+        order.description = prod.description
+        order.instrument_type = prod.product_type
+        order.instrument_handle = prod.handle
+        return order
+    
     success_url = '/orders/'
 
 class OrderDetailView(DetailView):
