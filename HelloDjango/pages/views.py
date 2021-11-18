721d6855bbee96e19b625989fb97a0f2b88bb131
Brett Edmonds <notquitealibrarian@gmail.com>
2021-11-18 17:27:46 -0600

    Added contect in home view to grab the 4 most ordered products, and html to display them in home.html.

--- HelloDjango/pages/views.py
+++ HelloDjango/pages/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from orders.models import Order
+from products.models import Product
 
 
 # This is where we keep our non-specific pages. Pages unique to a product/order are stored in the corresponding folder
 def home(request, *args, **kwargs):
 	latest_orders = Order.objects.order_by('-pk')[0:4]
 	most_reorders = Order.objects.order_by('-num_reorders')[0:4]
-	return render(request, "home.html", {'latest_orders': latest_orders, 'most_reorders': most_reorders})
+	featured_prods = Product.objects.order_by('-num_orders')[0:4]
+	return render(request, "home.html", {'latest_orders': latest_orders, 'most_reorders': most_reorders, 'featured_prods': featured_prods})
 	
 def products(request, *args, **kwargs):
 	return render(request, "catalogue.html", {})
