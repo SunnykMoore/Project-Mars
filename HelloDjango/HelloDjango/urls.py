019171c5994433c82d158d3642dde114afb95241
Brett Edmonds <notquitealibrarian@gmail.com>
2021-11-18 12:12:52 -0600

    Added url path for searching.

--- HelloDjango/HelloDjango/urls.py
+++ HelloDjango/HelloDjango/urls.py
@@ -30,5 +30,6 @@ urlpatterns = [
     path('order/create/', CreateOrderView.as_view(), name='create_order'),
     path('products/<prodID>/order/create/', OrderFromCatalog.as_view(), name='create_order'),
     path('orders/<int:pk>', OrderDetailView.as_view(), name='order_detail'),
-    path('orders/<pk>/reorder', OrderCopyView.as_view(), name='reorder')
+    path('orders/<pk>/reorder', OrderCopyView.as_view(), name='reorder'),
+    path('products/search', SearchCatalog.as_view(), name='search_catalog')
 ]
