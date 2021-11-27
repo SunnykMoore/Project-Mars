1dfe8309ccc7efa645e2abdc47cb27440b319c23
Brett Edmonds <notquitealibrarian@gmail.com>
2021-11-27 14:33:00 -0600

    Set media/root url in settings.py, added url for image files in urls.py, changed image link in catalog.

--- HelloDjango/HelloDjango/urls.py
+++ HelloDjango/HelloDjango/urls.py
@@ -19,6 +19,8 @@ from pages import views
 from orders.views import CurrentOrders, DeniedOrders, OrderFromCatalog, OrderListView, CreateOrderView, OrderDetailView, OrderCopyView, OrderApprove, OrderDeny, SearchOrders
 from products.views import SearchCatalog
 from products import views as prod
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,4 +38,4 @@ urlpatterns = [
     path('orders/current', CurrentOrders.as_view(), name='current_orders'),
     path('orders/denied', DeniedOrders.as_view(), name='denied_orders'),
     path('orders/search', SearchOrders.as_view(), name='search_orders')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
