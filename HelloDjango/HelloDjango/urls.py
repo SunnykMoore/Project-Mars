21909173a1f4f8637267a5f337df5655ac6caf72
Brett Edmonds <notquitealibrarian@gmail.com>
2021-11-26 21:49:51 -0600

    Added url for current orders, added link back to all orders on current order template.

--- HelloDjango/HelloDjango/urls.py
+++ HelloDjango/HelloDjango/urls.py
@@ -16,7 +16,7 @@ from django.contrib import admin
 from django.conf.urls import url,include
 from django.urls import path
 from pages import views
-from orders.views import OrderFromCatalog, OrderListView, CreateOrderView, OrderDetailView, OrderCopyView, OrderApprove, OrderDeny
+from orders.views import CurrentOrders, OrderFromCatalog, OrderListView, CreateOrderView, OrderDetailView, OrderCopyView, OrderApprove, OrderDeny
 from products.views import SearchCatalog
 from products import views as prod
 
@@ -33,5 +33,6 @@ urlpatterns = [
     path('orders/<pk>/reorder', OrderCopyView.as_view(), name='reorder'),
     path('orders/<int:pk>/approve', OrderApprove.as_view(), name='order_approve'),
     path('orders/<int:pk>/deny', OrderDeny.as_view(), name='order_deny'),
-    path('search/', SearchCatalog.as_view(), name='search_catalog')
+    path('search/', SearchCatalog.as_view(), name='search_catalog'),
+    path('orders/current', CurrentOrders.as_view(), name='current_orders')
 ]
