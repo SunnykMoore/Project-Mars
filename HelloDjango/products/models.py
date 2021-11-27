11865544a510f209bf78bdd0bb2f602471f956de
Brett Edmonds <notquitealibrarian@gmail.com>
2021-11-27 14:17:22 -0600

    Added media folder to store images.  Moved default image there.  Updated path to image.  Set upload_to path.

--- HelloDjango/products/models.py
+++ HelloDjango/products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
 	name = models.CharField(max_length = 360, default = "New Product")
 	description = models.TextField()
 	price = models.DecimalField(max_digits=100, decimal_places=2)
-	image = models.ImageField(default="default.jpeg")
+	image = models.ImageField(upload_to='media/', default="media/default.jpeg")
 	product_id = models.IntegerField(default=0000)
 	category = models.TextField(default = "")
 	size = models.CharField(max_length = 120, default = "Medium")
