f3ca8e6ce5ac0f3bbaeef803aa120f2dec57a949
Brett Edmonds <notquitealibrarian@gmail.com>
2021-12-02 17:14:08 -0600

    Merging in mars-122

--- HelloDjango/orders/models.py
+++ HelloDjango/orders/models.py
@@ -25,13 +25,37 @@ class Order(models.Model):
     )
     customer_type = models.CharField(max_length=10, choices=cust_choices, default="CURRENT")
     clinical_need = models.CharField(max_length=200)
-    instrument_category = models.CharField(max_length=70)
+    category_choices = (
+		("CT", "Cervical/Trauma"),
+		("IFAVBR", "Interbody Fusion and Vertebral Body Replacement"),
+		("TC", "Thoracolumbar"),
+		("MAST", "MAST"),
+		("GI", "General Instruments"),
+		("NP", "New Product")
+	)
+    instrument_category = models.CharField(max_length=6, choices=category_choices, null=True)
     description = models.TextField()
-    size = models.DecimalField(max_digits=5, decimal_places=2)
+    size = models.CharField(max_length = 120, default = "Standard")
     quantity = models.PositiveIntegerField()
     disclaimer = models.BooleanField()
-    instrument_type= models.CharField(max_length=270)
-    instrument_handle = models.CharField(max_length=270)
+    type_choices = (
+		("A", "Simple Modification - Make from Scratch"),
+		("B", "Minor Mod to Standard Device"),
+		("C", "Complex Design - Requires Predicate"),
+		("D", "Complex Assembly - Many Components"),
+		("NP", "New Product")
+	)
+    instrument_type = models.CharField(max_length=2, choices=type_choices, null=True)
+    handle_choices = (
+		("A","1.9 Inch Ball w/ Impact Cap"),
+		("B","4.8 Inch Ergonomic Inline"),
+		("C","4.75 Inch Tapered w/ Impact Cap"),
+		("D","6 Inch Ergonomic Inline w/ Impact Cap"),
+		("E","4 Inch Cervical Inline w/Impact Cap"),
+		("NP", "New Product"),
+		("NA", "Not Applicable")
+	)
+    instrument_handle = models.CharField(max_length=2, choices=handle_choices, null=True)
     status_choices = ( # Allowed status types
         ("SUBMITTED", "Submitted"),
         ("APPROVED", "Approved"),
