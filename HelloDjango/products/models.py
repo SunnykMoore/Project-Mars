7e902395f5ad4080693c6d7f3f78ae388985c0ab
Brett Edmonds <notquitealibrarian@gmail.com>
2021-12-02 22:15:31 -0600

    Merging.

--- HelloDjango/products/models.py
+++ HelloDjango/products/models.py
@@ -7,12 +7,37 @@ class Product(models.Model):
 	description = models.TextField()
 	price = models.DecimalField(max_digits=100, decimal_places=2)
 	image = models.ImageField(upload_to='media', default="default.jpeg")
-	product_id = models.IntegerField(default=0000)
-	category = models.TextField(default = "")
-	size = models.CharField(max_length = 120, default = "Medium")
-	handle = models.CharField(max_length = 360, default = "None")
-	product_type = models.CharField(max_length = 120, default = "Regular")
+	part_number = models.CharField(max_length = 360, default = "X0000000")
+	category_choices = (
+		("CT", "Cervical/Trauma"),
+		("IFAVBR", "Interbody Fusion and Vertebral Body Replacement"),
+		("TC", "Thoracolumbar"),
+		("MAST", "MAST"),
+		("GI", "General Instruments"),
+		("NP", "New Product")
+	)
+	category = models.CharField(max_length=6, choices=category_choices, null=True)
+	size = models.CharField(max_length = 120, default = "Standard")
+	handle_choices = (
+		("A","1.9 Inch Ball w/ Impact Cap"),
+		("B","4.8 Inch Ergonomic Inline"),
+		("C","4.75 Inch Tapered w/ Impact Cap"),
+		("D","6 Inch Ergonomic Inline w/ Impact Cap"),
+		("E","4 Inch Cervical Inline w/Impact Cap"),
+		("NP", "New Product"),
+		("NA", "Not Applicable")
+	)
+	handle = models.CharField(max_length=2, choices=handle_choices, null=True)
+	type_choices = (
+		("A", "Simple Modification - Make from Scratch"),
+		("B", "Minor Mod to Standard Device"),
+		("C", "Complex Design - Requires Predicate"),
+		("D", "Complex Assembly - Many Components"),
+		("NP", "New Product")
+	)
+	product_type = models.CharField(max_length=2, choices=type_choices, null=True)
 	num_orders = models.PositiveIntegerField(default=0)
+	department = models.CharField(max_length=20, blank=True, default='')
 	
 	def __str__(self):
 		return (self.name)
