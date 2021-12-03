153892d535c39aa03d00c7ab39a3cb283ed7954d
Brett Edmonds <notquitealibrarian@gmail.com>
2021-12-03 12:37:41 -0600

    The big plunge.  Renamed everything, we'll see if it still runs...

--- ProjectMars/ProjectMars/asgi.py
+++ ProjectMars/ProjectMars/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for HelloDjango project.
+ASGI config for ProjectMars project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HelloDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjectMars.settings')
 
 application = get_asgi_application()
